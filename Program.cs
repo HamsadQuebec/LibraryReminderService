091f0c52871c97ba921ff3fa1f6eb4f55b0ef106
HamsadQuebec <hamsadhoch@gmail.com>
2025-07-05 20:32:04 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-using LibraryEmailReminderWorker;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-await ReminderService.SendEmailReminders();
-
+namespace LibraryManagmentSystem_LMS_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
